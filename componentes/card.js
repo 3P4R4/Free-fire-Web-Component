ebe115098e01c3f8c3a4c24b548809d38c8d95b6
3p4r4 <mendoza124302@gmail.com>
2023-08-21 18:14:58 -0500

    custom properties

--- componentes/card.js
+++ componentes/card.js
@@ -1,79 +1,102 @@
-class productCard extends HTMLElement {
+class FreeFireCard extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: "open" });
   }
-  static get observedAttributes(){
-    return ["img", "movil", "title", "precio", "coleccion", "contenido"];
+  static get observedAttributes() {
+    return ["img", "movil", "titulo", "precio", "coleccion", "contenido", "alturaImg"];
   }
-  attributeChangedCallback(attr, odlval, newVal){
-    if(attr==="img"){
-        this.img = newVal;
+  attributeChangedCallback(attr, oldVal, newVal) {
+    if (attr === "img") {
+      this.img = newVal;
+      const pcImage = this.shadowRoot.querySelector(".pc");
+      if (pcImage) {
+        pcImage.src = this.img;
+      }
     }
-    if(attr==="movil"){
-        this.movil = newVal;
+    if (attr === "movil") {
+      this.movil = newVal;
+      const movilImage = this.shadowRoot.querySelector(".movil");
+      if (movilImage) {
+        movilImage.src = this.movil;
+      }
     }
-    if(attr==="title"){
-        this.title = newVal;
+    if (attr === "titulo") {
+      this.title = newVal;
+      const h2Element = this.shadowRoot.querySelector("h2");
+      if (h2Element) {
+        h2Element.textContent = this.title;
+      }
     }
-    if(attr==="precio"){
-        this.precio = newVal;
+    if (attr === "precio") {
+      this.precio = newVal;
     }
-    if(attr==="coleccion"){
-        this.coleccion = newVal;
+    if (attr === "coleccion") {
+      this.coleccion = newVal;
     }
-    if(attr==="contenido"){
-        this.contenido = newVal;
+    if (attr === "contenido") {
+      this.contenido = newVal;
+    }
+    if (attr === "alturaImg") {
+      this.alturaImg = newVal + 'px'; 
     }
   }
+  
   getTemplate() {
     const template = document.createElement("template");
     template.innerHTML = `
-            <main class="rgb">
-                <div class="container">
-                    <section class="imgBox">
-                        <img class="pc" src="${this.img}"/>
-                        <img class="movil" src="${this.movil}" />
-                        <img class="fCard" src="./img/fCard.png">
-                    </section>
-                    <section class="details">
-                        <div class="content">
-                            <h2>${this.title}<br><span>${this.coleccion}</span></h2>
-                            <p>${this.contenido}</p>
-                            <img class="fCard2" src="./img/fCard.png">
-                        </div>
-                    </section>
-                </div>
-            </main>
-            ${this.getStyles()};
-        `;
+    <main class="rgb">
+    <div class="container">
+    <section class="imgBox">
+    <img class="pc" src="${this.img}" style="height:${this.alturaImg}"/>
+    <img class="movil" src="${this.movil}" />
+    </section>
+    <section class="details">
+    <div class="content">
+    <h2>${this.title}<br><span>${
+      this.coleccion
+    }</span></h2>
+    <p>${this.contenido.substring(0, 100) + "..."}</p>
+    <img class="fCard2" src="./img/fCard.png">
+    <button>Ver mas</button>
+    </div>
+    </section>
+    </div>
+    </main>
+    ${this.getStyles()};
+    `;
     return template;
   }
   getStyles() {
+    
     return `
-        <style>
-        :host {
-        --primary-background: #171717;
-          width: 80%;
-          max-width: 900px;
-          min-width: 280px;
-          margin: 0 auto;
+    <style>
+    :host {
+      --primary-background: #171717;
+      --width: 500px;
+      --height: 300px;
+      --img-pc: 85%;
+      --img-movil: 100%;
+      --margin-movil: -25%;
+      --rgb-movil-top: 17%;
+      --btn-color: #fff;
+      --btn-font-size: 12px;
+      --btn-padding: 7px 13px
         }
         .container {
             position: relative;
             display: flex;
             flex-wrap: wrap;
             justify-content: space-between;
-            width: 900px;
-            height: 600px;
-            margin: 20px;
+            width: var(--width);
+            height: var(--height);
             background-color: transparent;
             border-radius: 10px;
             overflow: hidden;
             transform-style: preserve-3d;
-    perspective: 1000px;
-        }
-        .container .imgBox {
+            perspective: 1000px;
+          }
+          .container .imgBox {
             position: relative;
             display: flex;
             justify-content: center;
@@ -81,90 +104,86 @@ class productCard extends HTMLElement {
             height: 100%;
             background-color: var(--primary-background);
             border-radius: 10px 0 0 10px;
-      }
-      .container .imgBox:before {
-          position: absolute;
-          top: 20px;
-          left: 150px;
-          font-size: 8em;
-          letter-spacing: 15px;
-          font-weight: 800;
-          color: #fff;
-          content: 'Free';
-          opacity: 0.1;
-          font-family: free-fire;
-      }
+          }
+          .container .imgBox:before {
+            position: absolute;
+            top: 20px;
+            left: 45px;
+            font-size: 5em;
+            letter-spacing: 15px;
+            font-weight: 800;
+            color: #fff;
+            content: 'Free';
+            opacity: 0.1;
+            font-family: free-fire;
+          }
       .container .imgBox img.pc {
           position: relative;
-          top: 120px;
-          left: -40px;
-          width: 720px;
-          height: 480px;
+          top: 45px;
+          left: -20px;
+          width: auto;
+          height: var(--img-pc);
           transform: translateZ(20px);
-      }
-      .container .details {
+        }
+        .container .details {
           display: flex;
           justify-content: center;
-          align-items: center;
+          align-items: end;
           width: 50%;
           height: 100%;
           box-sizing: border-box;
-          padding: 40px;
+          padding: 15px;
           background-color: #fff;
           border-radius: 0 10px 10px 0;
-
-      }
-      .container .details:before {
-        position: absolute;
+          
+        }
+        .container .details:before {
+          position: absolute;
           top: 20px;
-          right: 170px;
-          font-size: 8em;
+          right: 55px;
+          font-size: 5em;
           letter-spacing: 15px;
           font-weight: 800;
           color: #000;
           content: 'Fire';
           opacity: 0.1;
           font-family: free-fire;
-      }
-      .container .details h2 {
-          margin-bottom: 20px;
-          font-size: 2.5em;
-          line-height: 0.8em;
+        }
+        .container .details h2 {
+          font-size: 2.3em;
+          line-height: 0.7em;
           color: #444;
           font-family: free-fire;
-      }
+          margin-bottom:5px
+          
+        }
       .container .details h2 span {
           font-size: 0.5em;
           text-transform: uppercase;
           letter-spacing: 2px;
           color: #999;
           font-family: Arial, Helvetica, sans-serif;
-          font-weight:bold
-      }
-      .container .details p {
+          font-weight:bold;
+        }
+        .container .details p {
           max-width: 100%;
-          margin-bottom: 35px;
           color: #333;
-          font-size: 18px;
+          font-size: 16px;
           transform: translateZ(30px);
-      }
-      .container .details h3 {
-          float: left;
-          font-size: 2.5em;
-          color: #a2a2a2;
+          margin-bottom:15px
       }
       .container .details button {
-          margin-top: 35px;
           float: right;
-          padding: 15px 20px;
-          font-size: 16px;
-          color: #fff;
+          padding: var(--btn-padding);
+          font-size: var(--btn-font-size);
+          color: var(--btn-color);
           letter-spacing: 1px;
           font-weight: 600;
           text-transform: uppercase;
-          border-radius: 40px;
-          background-color: #5a6cb2;
+          border-radius: 15px;
+          background-color: gray;
           cursor: pointer;
+          border: none
       }
       .container img.fCard {
         width: 180px;
@@ -203,10 +222,11 @@ class productCard extends HTMLElement {
         filter: blur(8px);
         z-index: -1;
         animation: rgb 6s linear infinite;
-        width: 905px;
-    height: 600px;
-    top: 3%;
-    left: 2%;
+        width: var(--width);
+        height: var(--height);
+          margin:0 auto;
+          top:33%
+    
       }
       @keyframes rgb {
         0% {background-position: 0% 50%;}
@@ -216,13 +236,12 @@ class productCard extends HTMLElement {
       .movil{
         display:none;
       }
-      @media (max-width: 1080px) {
+      @media (max-width: 600px) {
           .container {
-              height: auto;
-              width: auto;
+              height: 550px;
+              width: 350px;
           }
           .container .imgBox {
-              padding: 40px;
               width: 100%;
               box-sizing: border-box;
               height: auto;
@@ -238,9 +257,13 @@ class productCard extends HTMLElement {
           }
 
           .container .details {
+            display: flex;
+            justify-content: center;
+            align-items: center;
               width: 100%;
               height: auto;
-              padding: 20px;
+              padding: 30px 20px 0 20px;
+              border-radius: 0 0 10px 10px;
           }
 
           .container .details p {
@@ -252,27 +275,24 @@ class productCard extends HTMLElement {
         }
         .movil{
             display:block;
-            margin-bottom:-25%
-          }
-          .rgb::after {
-            width: 505px;
-            height: 702px;
-            top: 2%;
-            left: 4%;
+            margin-bottom:var(--margin-movil);
+            height:var(--img-movil);
+
           }
           .container .imgBox{
             padding: 10px 10px 0px
           }
-          .container .details {
-            padding: 70px 20px 0;
-            border-radius: 0 0 10px 10px;
-
-        }
+          .rgb::after {
+            top: var(--rgb-movil-top); 
+            width: 350px;
+        height: 550px;
+          margin:0 auto;
+          }
       }
       </style>
         `;
   }
-  
+
   render() {
     this.shadowRoot.appendChild(this.getTemplate().content.cloneNode(true));
   }
@@ -280,4 +300,4 @@ class productCard extends HTMLElement {
     this.render();
   }
 }
-customElements.define("ecommerce-card", productCard);
+customElements.define("mlz-ff", FreeFireCard);
